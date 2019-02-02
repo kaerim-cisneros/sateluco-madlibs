369f744766e586140b03c75a984f755710ed8038
kaerim-cisneros <kaerim.cisneros@gmail.com>
2019-02-02 08:48:21 -0700

    Change of the title in the index.html and in the app.js

--- src/components/app.js
+++ src/components/app.js
@@ -4,8 +4,8 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Bootstrap</h2>
+        <h1>Sateluco Madlibs Game</h1>
+        <h4>An exercise by Bottega</h4>
       </div>
     );
   }
