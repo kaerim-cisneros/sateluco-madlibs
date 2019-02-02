964b8ab929cecab665719ae76e0de68aaeae5452
kaerim-cisneros <kaerim.cisneros@gmail.com>
2019-02-02 09:25:59 -0700

    The writting of the first functional component

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className= "header">
+            <h1>Sateluco Madlibs Game</h1>
+            <h3>A Bottega Exercise</h3>
+            <p>Fill out the fields below and click the generate button to see the Mad Lib story.</p>
+        </div>
+    )
+}
+
+export default Header;
