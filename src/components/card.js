0549b64e52f3ce7ad1eff5e24cd9f7afab93bbc6
kaerim-cisneros <kaerim.cisneros@gmail.com>
2019-02-04 06:47:15 -0700

    Add botton and the ability to hide and show content

--- src/components/card.js
+++ src/components/card.js
@@ -24,13 +24,20 @@ class Card extends Component {
             adjectiveFour: '',
             nounThree: '',
             celebFour: '',
-            adjectiveFive: ''
+            adjectiveFive: '',
+            contentVisible: false
         }
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
 
     handleInputChange(event) {
         this.setState({ [event.target.name]: event.target.value})
+    }
+
+    handleFormSubmit(event) {
+        event.preventDefault()
+        this.setState({ contentVisible: !this.state.contentVisible})
     }
 
     render() {
@@ -58,12 +65,18 @@ class Card extends Component {
 
 
         return (
-            <div className = "card">
+            <form onSubmit = {this.handleFormSubmit} className = "card">
+                <div className = "card_inputs">
                 {
                     inputData.map(data => Input((data), this.handleInputChange))
                 }
-                <Content data={this.state}/>
-            </div>
+                </div>
+                <button type="submit" > {!this.state.contentVisible ? "Generate Mad Libs" : "Clear Form"} </button>
+                {
+                    this.state.contentVisible ? <Content data = {this.state}/> : ""
+                }
+                
+            </form>
             
         )
     }
