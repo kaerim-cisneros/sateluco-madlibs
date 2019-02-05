a5390b21bbbf7e42e03ba023b564dd3f3e940dfa
kaerim-cisneros <kaerim.cisneros@gmail.com>
2019-02-05 07:28:51 -0700

    add grid structure to the card

--- src/components/input.js
+++ src/components/input.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Input = ({title, state, name},onChange, index) => {
     return (
         <div key={name} className= "input">
-           <label className = "input_number">{index +1}</label>
+           <label className = "input-number">{index +1}</label>
            <input name={name} value= {state} onChange= {onChange}/>
-           <label className = "inpit_title">{title}</label>
+           <label className = "input-title">{title}</label>
         </div>
     )
 }
