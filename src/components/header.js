22cf9b85aecad7c208a89c230de5f03da1188da3
kaerim-cisneros <kaerim.cisneros@gmail.com>
2019-02-05 22:40:00 -0700

    Add skew as background

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <div className= "header">
+            <div className= "header-skew"></div>
+            <div className= "header-second-skew"></div>
             <h1>Sateluco Madlibs Game</h1>
             <h3>A Bottega Exercise</h3>
             <p>Fill out the fields below and click the generate button to see the Mad Lib story.</p>
