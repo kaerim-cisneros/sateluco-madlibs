78c995658ec51dbe4dd6c53fba1028b4cf7b9195
kaerim-cisneros <kaerim.cisneros@gmail.com>
2019-02-10 08:12:00 -0700

    final version

--- src/components/input.js
+++ src/components/input.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Input = ({title, state, name},onChange, index) => {
     return (
         <div key={name} className= "input">
-           <label className = "input-number">{index +1}</label>
-           <input name={name} value= {state} onChange= {onChange}/>
+           <label className = {`input-number ${state == '' ? 'gray' : 'blue'}`}>{index + 1}</label>
+           <input name={name} value={state} onChange={onChange}/>
            <label className = "input-title">{title}</label>
         </div>
     )
